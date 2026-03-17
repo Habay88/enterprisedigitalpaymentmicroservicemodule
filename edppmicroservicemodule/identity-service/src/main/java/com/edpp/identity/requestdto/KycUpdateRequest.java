0a550a72d0f5dd676521ee039196b8a51a503132
Banjoko A.L <56545809+Habay88@users.noreply.github.com>
2026-03-17 15:38:30 +0100

    application properties configuration

--- edppmicroservicemodule/identity-service/src/main/java/com/edpp/identity/requestdto/KycUpdateRequest.java
+++ edppmicroservicemodule/identity-service/src/main/java/com/edpp/identity/requestdto/KycUpdateRequest.java
@@ -11,6 +11,10 @@ import java.time.LocalDateTime;
 
 import com.edpp.identity.model.KycDetails;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 @Data
 @Builder
 @NoArgsConstructor
