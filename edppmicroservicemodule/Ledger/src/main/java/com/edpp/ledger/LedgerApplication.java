18f86e17d539c679b4a8393d9ecd110f2be132c1
Banjoko A.L <56545809+Habay88@users.noreply.github.com>
2026-04-08 16:18:43 +0100

    ledger entity

--- edppmicroservicemodule/Ledger/src/main/java/com/edpp/ledger/LedgerApplication.java
+++ edppmicroservicemodule/Ledger/src/main/java/com/edpp/ledger/LedgerApplication.java
@@ -2,8 +2,18 @@ package com.edpp.ledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
+@EnableKafka
+@EnableAsync
+@EnableScheduling
 public class LedgerApplication {
 
 	public static void main(String[] args) {
